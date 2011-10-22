36ce77167d46536024e873b5e5784b995d551ceb
Igor Sutton <igorsutton@me.com>
2011-10-22 10:35:33 +0200

    Initial Commit

---
+++ Image Slicer/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Image Slicer
+//
+//  Created by Igor Sutton on 10/22/11.
+//  Copyright (c) 2011 igorsutton.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
